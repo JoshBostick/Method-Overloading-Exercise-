8293ffaadcc6d1b21e73a003a9d7ecc77fe9dc40
Josh Bostick <jbostick99@gmail.com>
2023-11-30 13:28:09 -0600

    finished

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace MethodOverloading
+﻿using System.Reflection;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer1 = Add(0,1,true);
+            Console.WriteLine(answer1);
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool dollars)
+        {
+            if (dollars && (a + b) !=1)
+            {
+                return $"{a + b} dollars";
+            }
+            else if (dollars && (a + b) == 1)
+            {
+                return $"{a + b} dollar";
+            }
+            else
+            {
+                return $"{a + b}";  
+            }
+
         }
     }
 }
