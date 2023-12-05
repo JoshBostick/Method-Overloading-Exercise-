b3032053d8c21cf809aa686cf04974e6f0b7dbb9
Josh Bostick <jbostick99@gmail.com>
2023-12-05 10:57:59 -0600

    finished

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,8 +6,12 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
-            var answer1 = Add(0,1,true);
+            var answer1 = Add(0, 1, true);
             Console.WriteLine(answer1);
+            var answer2 = Add(2,3);
+            Console.WriteLine(answer2);
+            var answer3 = Add(3.3m,4.1m);
+            Console.WriteLine(answer3);
         }
         public static int Add(int a, int b)
         {
@@ -18,20 +22,20 @@ namespace MethodOverloading
             return a + b;
         }
         public static string Add(int a, int b, bool dollars)
-        {
-            if (dollars && (a + b) !=1)
             {
-                return $"{a + b} dollars";
+                if (dollars && (a + b) != 1)
+                {
+                    return $"{a + b} dollars";
+                }
+                else if (dollars && (a + b) == 1)
+                {
+                    return $"{a + b} dollar";
+                }
+                else
+                {
+                    return $"{a + b}";
+                }
             }
-            else if (dollars && (a + b) == 1)
-            {
-                return $"{a + b} dollar";
-            }
-            else
-            {
-                return $"{a + b}";  
-            }
-
-        }
+        
     }
 }
